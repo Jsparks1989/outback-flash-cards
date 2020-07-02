408a400d5f45510eb0e63956c07cd6b55e35e7f5
Jsparks1989 <jsparks19891@gmail.com>
2020-07-02 02:49:40 -0400

    The base set up for application

---
+++ flash-cards/src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+class App extends React.Component {
+    render() {
+        return(
+            <div className='ui container'>
+                App Component
+            </div>
+        );
+    }
+}
+
+
+
+export default App;
