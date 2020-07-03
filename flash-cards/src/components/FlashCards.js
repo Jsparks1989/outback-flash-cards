5b39dd9279b2a9f4b510a44768e584f26fffe286
Jsparks1989 <jsparks19891@gmail.com>
2020-07-03 02:28:09 -0400

    cleaning up comments.

--- flash-cards/src/components/FlashCards.js
+++ flash-cards/src/components/FlashCards.js
@@ -6,18 +6,11 @@ class FlashCards extends React.Component {
 
 
     randomNumber = 0;
-    //-- Only needs to return a random number between 0 and array.length-1. This number will be used as the index number for the array
-    //   and will determine the QA object that will be displayed.
-    randomQuestion = (arry) => {
-        //-- num is generating the random number for the array index. Now 
-        //   need to figure out how to insert the number into props.questionAnswer[] for 
-        //   the index.
-        const num = Math.floor(Math.random() * arry.length);
-        // this.randomNumber = num;
-        // console.log(this.randomNumber);
-        const display = arry[Math.floor(Math.random() * arry.length)];
-        console.log(num);
-       //return display;
+
+    randomQuestion = (arry=[]) => {
+        var num = Math.floor(Math.random() * arry.length);
+        //const display = arry[Math.floor(Math.random() * arry.length)];
+        this.randomNumber = num;
     }
 
 
@@ -34,20 +27,11 @@ class FlashCards extends React.Component {
       }
 
 
-    //-- Next button not working. Try having the array of questions displayed and have the next button
-    //   seed the array with a random array index number.
-    //-- I need a question/answer object to be displayed immediately when component loads (possibly use componentDidMount() https://www.w3schools.com/react/react_lifecycle.asp).
-    //   Once the initial question/answer object loads, I need the Next button to pick a random question/answer object from the array to display next.
-
-    //-- Trying to seed the array index with randomNumber. randomNumber is being set to the 
-
-
     render() {
         console.log(this.randomQuestion().num);
         return(
             <div className='ui container'>
                 <div id='question'>
-                {/* {this.randomQuestion(this.props.questionAnswer)} */}
                 { this.props.questionAnswer[this.randomNumber].question }
                 </div>
                 <div id='answer' style={{ display: 'none' }}>
